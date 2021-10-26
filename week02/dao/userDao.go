1e1a1ba37be83dcd8f6700a48fcaa79c77300272
Vincent <linhuanbo@grgbanking.com>
2021-10-26 15:20:35 +0800

    docs: dao WithMessage err
    Signed-off-by: Vincent <linhuanbo@grgbanking.com>

--- week02/dao/userDao.go
+++ week02/dao/userDao.go
@@ -20,10 +20,18 @@ func (u *User) GetUser(id int) (*User, error) {
 	err := DBConn.QueryRow("SELECT id, name FROM users WHERE id in=(?)", id).Scan(&user.ID, &user.Name)
 	if err != nil {
 		if errors.Is(err,sql.ErrNoRows) {
-			// sql.ErrNoRows 如果上层不关心数据是否为空的场景
-			return &user, nil
-			// 如果是第三方调用或上层关注数据是否为空的场景,返回如下
-			//return nil, errors.Wrap(err, "userDao: sql.ErrNoRows")
+			// begin
+
+			// ① sql.ErrNoRows 如果上层不关心数据是否为空的场景
+			//return &user, nil
+
+			// ② 如果是第三方调用或上层关注数据是否为空的场景,返回如下
+			// 需要附加调用栈：
+			return nil, errors.Wrap(err, "userDao: sql.ErrNoRows")
+			// 不需要附加调用栈，但要附加上下文信息：
+			//return nil, errors.WithMessage(err, "userDao: sql no rows found")
+
+			// end
 		} else {
 			// 其他错误， Warp 后交给上一层，或者记录一下日志然后返回空 + 错误
 			return nil, errors.Wrap(err, "userDao: get users failed")
